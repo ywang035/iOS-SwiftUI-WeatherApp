652069c76b2c1a6db7ba050f57bc294e4a86f1a2
Y. WANG <49180491+ywang035@users.noreply.github.com>
2021-09-07 15:43:34 +1000

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Yang W on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
