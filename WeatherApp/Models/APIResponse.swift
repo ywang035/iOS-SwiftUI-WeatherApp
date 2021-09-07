2def17e90015885d5dd5bdebc083957243a6276a
Y. WANG <49180491+ywang035@users.noreply.github.com>
2021-09-07 17:28:26 +1000

    add API response meta data struct, decode API response, add weather data struct

---
+++ WeatherApp/Models/APIResponse.swift
@@ -0,0 +1,64 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Yang W on 7/9/21.
+//
+
+import Foundation
+
+/// api response
+struct AIPResponse: Codable {
+    var cnt: Int
+    var list: [DataList]
+}
+
+/// meta data list
+struct DataList: Codable {
+    var coord: Coordinator
+    var sys: SysInfo
+    var weather: [MinorWeather]
+    var main: MainWeather
+    var visibility: Int
+    var wind: Wind
+    var clouds: Cloud
+    var dt, id: Int
+    var name: String
+    
+}
+
+
+/// meta data main weather
+struct MainWeather: Codable {
+    var temp, feels_like, temp_min, temp_max: Double
+    var pressure, humidity: Int
+}
+
+
+/// meta data minor weather
+struct MinorWeather: Codable {
+    var id: Int
+    var main, description, icon: String
+}
+
+/// meta data wind
+struct Wind: Codable {
+    var speed: Double
+    var deg: Int
+}
+
+/// meta data cloud
+struct Cloud: Codable {
+    var all: Int
+}
+
+/// meta data system info( time zone, sun info)
+struct SysInfo: Codable {
+    var country: String
+    var timezone, sunrise, sunset: Int
+}
+
+/// meta data coordinator
+struct Coordinator: Codable {
+    var lon, lat: Double
+}
--- WeatherApp/Views/MainView.swift
+++ WeatherApp/Views/MainView.swift
@@ -9,12 +9,44 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Button(action: {
+                APICaller().loadData { value in
+                    print(value)
+                }
+            }, label: {
+                Text("get API")
+            })
+        }
+        
+        
     }
 }
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
+    }
+}
+
+
+
+
+
+class APICaller : ObservableObject{
+    
+    func loadData(completion:@escaping (AIPResponse) -> ()) {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/group?id=2158177,2147714&appid=d36df9373c1eb937af289bc9b4685c80") else {
+            print("Invalid url...")
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            let decodeResult = try! JSONDecoder().decode(AIPResponse.self, from: data!)
+                        DispatchQueue.main.async {
+                            completion(decodeResult)
+                        }
+        }.resume()
+        
     }
 }
